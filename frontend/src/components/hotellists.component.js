e8a02024e1652fafae999eea444d8a915020ddc9
ohmpatell <ohm2003.patel@gmail.com>
2023-11-16 01:19:58 -0500

    Refactor hotel list component to use HotelCard component

--- frontend/src/components/hotellists.component.js
+++ frontend/src/components/hotellists.component.js
@@ -1,16 +1,6 @@
 import axios from "axios";
 import React, { Component } from "react";
-
-const Hotel = (props) => (
-  <tr>
-    <td>{props.hotel.name}</td>
-    <td>{props.hotel.location}</td>
-    <td>{props.hotel.phone}</td>
-    <td>{props.hotel.email}</td>
-    <td>{props.hotel.numberOfRooms}</td>
-    <td>{props.hotel.description}</td>    
-  </tr>
-);
+import HotelCard from "./hotelCard.component"; 
 
 export default class HotelLists extends Component {
   constructor(props) {
@@ -32,27 +22,13 @@ export default class HotelLists extends Component {
       .catch((err) => console.log(err));
   }
 
-  HotelLists() {
-    return this.state.hotels.map(function (currentHotel, i) {
-      return <Hotel hotel={currentHotel} key={i} />;
-    });
+  renderHotelCards() {
+    return this.state.hotels.map((currentHotel, i) => (
+      <HotelCard hotel={currentHotel} key={i} />
+    ));
   }
 
   render() {
-    return (
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Location</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">Number of rooms</th>
-            <th scope="col">Description</th>
-          </tr>
-        </thead>
-        <tbody>{this.HotelLists()}</tbody>
-      </table>
-    );
+    return <div>{this.renderHotelCards()}</div>;
   }
 }
