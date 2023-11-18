a35424a88fbe5c96333fab285a2c566ee8a270dc
ohmpatell <ohm2003.patel@gmail.com>
2023-11-18 14:35:37 -0500

    Added the Image functionality

--- Backend/app/routes/index.server.routes.js
+++ Backend/app/routes/index.server.routes.js
@@ -1,53 +1,77 @@
 const express = require('express');
+const multer = require('multer');
 const hotelRouter = express.Router();
 const Hotel = require('../model/hotel.model');
 
-hotelRouter.route('/hotel').get((req, res, next) =>
-{
-    Hotel.find().then(object => res.status(200).json(object))
-        .catch(err => res.status(400).json({ "error": err }))
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Get all hotels
+hotelRouter.route('/hotel').get((req, res, next) => {
+    Hotel.find()
+        .then(hotels => res.status(200).json(hotels))
+        .catch(err => res.status(400).json({ "error": err }));
 });
 
-hotelRouter.route('/hotel/:id').get((req, res, next) =>
-{
-    Hotel.findById(req.params.id).then(object => res.status(200).json(object))
-        .catch(err => res.status(400).json({ "error": err }))
-});
-
-hotelRouter.route('/hotel/add').post((req, res, next) =>
-{
-    let hotel = new Hotel(req.body)
-    hotel.save()
-        .then(object => res.status(200).json(object))
-        .catch(err => res.status(400).json({ "error": err }))
-});
-
-hotelRouter.route('/hotel/update/:id').post((req, res, next) =>
-{
+// Get a hotel by ID
+hotelRouter.route('/hotel/:id').get((req, res, next) => {
     Hotel.findById(req.params.id)
-        .then(object =>
-        {
-            //Update the object with new data
-            object.name = req.body.name;
-            object.location = req.body.location;
-            object.phone = req.body.phone;
-            object.email = req.body.email;
-            object.description = req.body.description;
-            object.numberOfRooms = req.body.numberOfRooms;        
-            //Save new Data
-
-            object.save()
-                .then(object => res.status(200).json(object))
-                .catch(err => res.status(400).json({ "error": err }))            
-        })
-        .catch(err => res.status(400).json({ "error": err }))
+        .then(hotel => res.status(200).json(hotel))
+        .catch(err => res.status(400).json({ "error": err }));
 });
 
-hotelRouter.route('/hotel/delete/:id').delete((req, res, next) =>
-{
+// Add a new hotel with image upload
+hotelRouter.route('/hotel/add').post(upload.single('image'), (req, res, next) => {
+    const { name, location, phone, email, description, numberOfRooms } = req.body;
+
+    let hotel = new Hotel({
+        name,
+        location,
+        phone,
+        email,
+        description,
+        numberOfRooms,
+        // Check if a file is uploaded
+        image: req.file ? req.file.buffer : undefined,
+    });
+
+    hotel.save()
+        .then(savedHotel => res.status(200).json(savedHotel))
+        .catch(err => res.status(400).json({ "error": err }));
+});
+
+// Update a hotel by ID with image upload
+hotelRouter.route('/hotel/update/:id').post(upload.single('image'), (req, res, next) => {
+    const { name, location, phone, email, description, numberOfRooms } = req.body;
+
+    Hotel.findById(req.params.id)
+        .then(hotel => {
+            // Update the hotel with new data
+            hotel.name = name;
+            hotel.location = location;
+            hotel.phone = phone;
+            hotel.email = email;
+            hotel.description = description;
+            hotel.numberOfRooms = numberOfRooms;
+
+            // Check if a file is uploaded
+            if (req.file) {
+                hotel.image = req.file.buffer; // Save the image as a Buffer
+            }
+
+            // Save new Data
+            hotel.save()
+                .then(updatedHotel => res.status(200).json(updatedHotel))
+                .catch(err => res.status(400).json({ "error": err }));
+        })
+        .catch(err => res.status(400).json({ "error": err }));
+});
+
+// Delete a hotel by ID
+hotelRouter.route('/hotel/delete/:id').delete((req, res, next) => {
     Hotel.findByIdAndDelete(req.params.id)
-        .then(object => res.status(200).send("Registry deleted successfully"))
-        .catch(err => res.status(400).json({ "error": err }))
+        .then(() => res.status(200).send("Hotel deleted successfully"))
+        .catch(err => res.status(400).json({ "error": err }));
 });
 
 module.exports = hotelRouter;
--- frontend/src/components/hotelCard.component.js
+++ frontend/src/components/hotelCard.component.js
@@ -1,18 +1,45 @@
 import React from "react";
 
-const HotelCard = ({ hotel }) => (
-  <div className="container mt-5">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{hotel.name}</h5>
-        <p className="card-text">Location: {hotel.location}</p>
-        <p className="card-text">Phone: {hotel.phone}</p>
-        <p className="card-text">Email: {hotel.email}</p>
-        <p className="card-text">Number of Rooms: {hotel.numberOfRooms}</p>
-        <p className="card-text">Description: {hotel.description}</p>
+const HotelCard = ({ hotel }) => {
+  const getImageSrc = () => {
+    if (hotel.image && hotel.image.data) {
+      const arrayBufferView = new Uint8Array(hotel.image.data);
+      const blob = new Blob([arrayBufferView], { type: "image/jpeg" });
+      const imageUrl = URL.createObjectURL(blob);
+      return imageUrl;
+    }
+    return null;
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="card">
+        <div className="card-body d-flex justify-content-between">
+          {/* Left Side: Information */}
+          <div style={{ maxWidth: "70%" }}>
+            <h5 className="card-title">{hotel.name}</h5>
+            <p className="card-text">Location: {hotel.location}</p>
+            <p className="card-text">Phone: {hotel.phone}</p>
+            <p className="card-text">Email: {hotel.email}</p>
+            <p className="card-text">Number of Rooms: {hotel.numberOfRooms}</p>
+            <p className="card-text">Description: {hotel.description}</p>
+          </div>
+
+          {/* Right Side: Image */}
+          {hotel.image && (
+            <div style={{ maxWidth: "30%", 
+            borderRadius: "10px"}}>
+              <img
+                src={getImageSrc()}
+                alt="Hotel"
+                style={{ maxWidth: "100%" }}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HotelCard;
