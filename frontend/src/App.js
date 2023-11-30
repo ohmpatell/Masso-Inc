8c902138c28630200f4a0b8f5494b1a843cff42f
SantiCarreno9 <santiago.carreno05@gmail.com>
2023-11-30 14:49:11 -0500

    Signup, login and logout implemented and integrated with frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,23 @@
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar";
 import AddHotel from "./components/addhotel.component";
 import HotelLists from "./components/hotellists.component";
 import LandingPage from "./components/landingpage.component";
+import Login from "./components/login.component";
+import Registration from "./components/registration.component";
 
 function App() {
   return (
     <Router>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          <img
-            src=".\logo.png"
-            alt="Logo"
-            style={{ width: "80px", marginLeft: "10px" }}
-          />
-        </Link>
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <Link class="nav-link" to="/">
-              Home
-            </Link>
-          </li>
-          <li class="nav-item active">
-            <Link class="nav-link" to="/hotel/add">
-              Add Hotel
-            </Link>
-          </li>
-          <li class="nav-item active">
-            <Link class="nav-link" to="/hotel">
-              Hotel List
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar/>
       <Routes>
         <Route path="/" Component={LandingPage} />
         <Route path="/hotel/add" Component={AddHotel} />
         <Route path="/hotel" Component={HotelLists} />
+        <Route path="/login" Component={Login} />
+        <Route path="/signup" Component={Registration} />
       </Routes>
     </Router>
   );
